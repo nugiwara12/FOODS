f7117fbe7d3a0b099f56229373db42878b60d407
Nugiwara_011200 <global-config@example.com>
2024-05-20 00:47:40 +0800

    add foodhub description

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -96,11 +96,7 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm sm:text-center">
-            © 2023{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
-              Flowbite™
-            </a>
-            . All Rights Reserved.
+            ©FoodHub . All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
             <a href="#" className="text-white hover:text-yellow-600 ms-5">
